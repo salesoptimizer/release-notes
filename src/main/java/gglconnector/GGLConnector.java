362532cfbb6fa3ec5bf5cdea2c53e2319cb5f64e
zgoryn <yevhenii.riabinin@gmail.com>
2015-05-12 17:57:17 +0300

    Add *.p12

--- src/main/java/gglconnector/GGLConnector.java
+++ src/main/java/gglconnector/GGLConnector.java
@@ -8,6 +8,7 @@ import java.util.Collections;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.drive.Drive;
