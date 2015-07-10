d44029268335590d2158bd60eb0db512464a75c4
zgoryn <yevhenii.riabinin@gmail.com>
2015-07-10 17:31:58 +0300

    refactoring of SFQuery

--- src/main/java/sfconnector/SFQuery.java
+++ src/main/java/sfconnector/SFQuery.java
@@ -135,9 +135,10 @@ public class SFQuery {
 		params[0] = new NameValuePair("q",
 				"SELECT Name, Id, Fixed_in_Ver__c, Release_Notes__c "
 			  + "FROM Ticket__c "
-			  + "WHERE (Fixed_in_Ver__c >= '" + ver1 + "' AND Fixed_in_Ver__c <= '" + ver2 + "')"
-			  + "AND Project__c = '" + projectId + "'"
-			  + "AND Release_Notes__c != ''"
+			  + "WHERE Release_Notes__c != ''"
+//			  + "WHERE (Fixed_in_Ver__c >= '" + ver1 + "' AND Fixed_in_Ver__c <= '" + ver2 + "')"
+//			  + "AND Project__c = '" + projectId + "'"
+//			  + "AND Release_Notes__c != ''"
 	  		  + "LIMIT 100");
 		GetMethod getMethod = createGetMethod();
 		getMethod.setQueryString(params);
