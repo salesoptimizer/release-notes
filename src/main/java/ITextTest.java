cf65eef63e071fb46c04b529508e45aa5c9c0097
zgoryn <yevhenii.riabinin@gmail.com>
2015-07-10 19:40:31 +0300

    refactoring of SFQuery

--- src/main/java/ITextTest.java
+++ src/main/java/ITextTest.java
@@ -3,22 +3,37 @@ import java.io.FileOutputStream;
 
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.Element;
+import com.lowagie.text.PageSize;
 import com.lowagie.text.Phrase;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
 import com.lowagie.text.rtf.RtfWriter2;
 
 
 public class ITextTest {
 
 	public static void main(String[] args) {
-		 Document document = new Document();
+		Document document = new Document(PageSize.A4);
 	        try {
-	            RtfWriter2 writer = RtfWriter2.getInstance(document, new FileOutputStream(
-	                    "RTFExamplePdfPTable1.rtf"));
+	            PdfWriter.getInstance(document, new FileOutputStream("PDFExamplePdfPTable1.pdf"));
 	            document.open();
 	          
-	            PdfPTable t1 = new PdfPTable(3);
+	            PdfPTable table = new PdfPTable(2);
+	            table.setWidthPercentage(100);
+	            // first movie
+	            table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
+	            table.getDefaultCell().setVerticalAlignment(Element.ALIGN_TOP);
+	            table.addCell("X-Men");
+	            
+	            table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_RIGHT);
+	            table.getDefaultCell().setVerticalAlignment(Element.ALIGN_TOP);
+	            table.addCell("X-Men");
+	            
+	            table.completeRow();
+	            
+	           /* PdfPTable t1 = new PdfPTable(3);
 	            t1.setTotalWidth(new float[] {20.0f, 50.0f, 30.0f});
 	            PdfPCell c = new PdfPCell(new Phrase("Entry T1.R1.C1"));
 	            t1.addCell(c);
@@ -33,10 +48,10 @@ public class ITextTest {
 	            c = new PdfPCell(new Phrase(""));
 	            t1.addCell(c);
 	            c = new PdfPCell(new Phrase("Entry T1.R2.C2"));
-	            t1.addCell(c);           
-	            t1.completeRow();
+	            t1.addCell(c);            
+	            t1.completeRow();*/
 	           
-	            document.add(t1);
+	            document.add(table);
 	           
 	        } catch (FileNotFoundException e) {
 	            e.printStackTrace();
--- src/main/java/rnservices/RTFConverter.java
+++ src/main/java/rnservices/RTFConverter.java
@@ -8,10 +8,12 @@ import java.util.List;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
 import com.lowagie.text.Element;
+import com.lowagie.text.PageSize;
 import com.lowagie.text.Phrase;
 import com.lowagie.text.html.HtmlParser;
 import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
 import com.lowagie.text.rtf.RtfWriter2;
 
 import models.ReleaseNote;
@@ -20,40 +22,50 @@ public class RTFConverter {
 //	private List<ReleaseNote> releaseNotes;
 	
 	public static boolean convertToRTF(List<ReleaseNote> releaseNotes) {
-		Document document = new Document();
+		Document document = new Document(PageSize.A4);
         try {
-            RtfWriter2 writer = RtfWriter2.getInstance(document, new FileOutputStream(
-                    "ReleaseNotes.rtf"));
+//            RtfWriter2 writer = RtfWriter2.getInstance(document, new FileOutputStream("ReleaseNotes.rtf"));
+        	PdfWriter.getInstance(document, new FileOutputStream("ReleaseNotes.pdf"));
             document.open();
           
-            PdfPTable t1 = new PdfPTable(3);
-            t1.setTotalWidth(new float[] {20.0f, 70.0f, 30.0f});
+            PdfPTable table = new PdfPTable(3);
+            table.setTotalWidth(new float[] {20.0f, 70.0f, 30.0f});
+            // first movie
+            table.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
+            table.getDefaultCell().setVerticalAlignment(Element.ALIGN_TOP);
+            table.addCell("Ticket name");
+            table.addCell("Ticket release notes");
+            table.addCell("Fix.version");
+            table.completeRow();
             
-            t1.getDefaultCell().setHorizontalAlignment(Element.ALIGN_CENTER);
-            t1.getDefaultCell().setVerticalAlignment(Element.ALIGN_TOP);
-            t1.addCell("Ticket name");
-            PdfPCell c = new PdfPCell(new Phrase("Ticket release notes"));
+            /*PdfPTable t1 = new PdfPTable(3);
+//            t1.setTotalWidth(new float[] {20.0f, 50.0f, 30.0f});
+            
+            PdfPCell c = new PdfPCell(new Phrase("Ticket name"));
+            
+            t1.addCell(c);
+            c = new PdfPCell(new Phrase("Ticket release notes"));
             t1.addCell(c);           
             c = new PdfPCell(new Phrase("Fix.version"));
             t1.addCell(c);           
             t1.completeRow();
             
-            HtmlParser hp = new HtmlParser();
+            HtmlParser hp = new HtmlParser();*/
             
             Iterator<ReleaseNote> iterator = releaseNotes.iterator();
             ReleaseNote rnote;
             while (iterator.hasNext()) {
             	rnote = iterator.next();
-	            c = new PdfPCell(new Phrase(rnote.getTicketName()));
-	            t1.addCell(c);
+            	PdfPCell c = new PdfPCell(new Phrase(rnote.getTicketName()));
+	            table.addCell(c);
 	            c = new PdfPCell(new Phrase(rnote.getReleaseNotes()));
-	            t1.addCell(c);           
+	            table.addCell(c);           
 	            c = new PdfPCell(new Phrase(rnote.getPackVersion()));
-	            t1.addCell(c);           
-	            t1.completeRow();
+	            table.addCell(c);           
+	            table.completeRow();
             }
            
-            document.add(t1);
+            document.add(table);
            
         } catch (FileNotFoundException e) {
             e.printStackTrace();
