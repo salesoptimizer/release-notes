bdbb872db4c856c1e4ba47ff2a1f7f795e9d5630
zgoryn <yevhenii.riabinin@gmail.com>
2015-07-17 12:41:12 +0300

    response to sf

--- src/main/java/sfconnector/SFConnector.java
+++ src/main/java/sfconnector/SFConnector.java
@@ -64,6 +64,7 @@ public class SFConnector/* extends HttpServlet*/ {
 	public void getAccessToSalesforce(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String accessToken = (String) request.getSession().getAttribute(ACCESS_TOKEN);
 		PrintWriter out = response.getWriter();
+		out.println("URI => " + request.getRequestURI());
 		if (accessToken == null) {
 			String instanceUrl = null;
 			/*if (!request.getRequestURI().endsWith("_callback")) {
