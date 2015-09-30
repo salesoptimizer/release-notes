44a65d969f515edf3daf4dbf634de38e91aace02
zgoryn <yevhenii.riabinin@gmail.com>
2015-09-30 13:08:55 +0300

    test

--- src/main/java/rncontroller/RNController.java
+++ src/main/java/rncontroller/RNController.java
@@ -102,16 +102,16 @@ public class RNController extends HttpServlet {
 			GGLService.docName = sfQuery.getProjectName(this.projectId);
 			
 			if (GGLService.createGoogleDoc()) {
-				response.getWriter().println("<b>Release Notes document was successfully created on Google Drive</b>");
+				response.getWriter().println("<b>Release Notes document was successfully created on Google Drive</b>\n");
 			} else {
-				response.getWriter().println("<b>Error during document creating. Please, check app logs for getting more info</b>");
+				response.getWriter().println("<b>Error during document creating. Please, check app logs for getting more info</b>\n");
 			}
 			
 			RTFConverter.convertToRTF(tickets, logo);
 			if (sfQuery.addAttachmentToProject(this.projectId)) {
-				response.getWriter().println("<b>Release Notes document was successfully added to the Project's attachments</b>");
+				response.getWriter().println("<b>Release Notes document was successfully added to the Project's attachments</b>\n");
 			} else {
-				response.getWriter().println("<b>Error during document creating. Please, check app logs for getting more info</b>");
+				response.getWriter().println("<b>Error during document creating. Please, check app logs for getting more info</b>\n");
 			}
 		}
 	}
